1f848af92df3dd6a564fe2722a3c1054e2d5487b
Lenur Serveriiev <lenur.serveriev@gmail.com>
2024-12-29 19:11:27 +0100

    Init project

---
+++ src/main/java/de/lenur/realtime/RealtimeApplication.java
@@ -0,0 +1,13 @@
+package de.lenur.realtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeApplication.class, args);
+    }
+
+}
